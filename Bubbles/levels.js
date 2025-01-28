e2e56190c871170c5aad0f64beda2cc60b6f3ee5
Youness El Andaloussi <youness@elandaloussi.net>
2025-01-28 23:39:04 +0100

    Bubbles/

--- Bubbles/levels.js
+++ Bubbles/levels.js
@@ -1,17 +1,224 @@
 export const levels = [
-  {
+
+
+  { //level 1
+  grid: `
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+  BB                       BB
+  BB                       BB
+  BB      +                BB
+  BB                       BB
+  BB        +              BB
+  BB           +           BB
+  BB                       BB
+  BBBB  BBBBBBBBBBBBBBB  BBBB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BBBB  BBBBBBBBBBBBBBB  BBBB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BBBB  BBBBBBBBBBBBBBB  BBBB
+  BB                       BB
+  BB                       BB
+  BB1                      BB
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+  `,
+  jumpHeight: 2
+    },
+    { //level2
+      jumpHeight: 2,
+      grid: `
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+  BB                       BB
+  BB                       BB
+  BB     +                 BB
+  BB    +                  BB
+  BB      + BBBB           BB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BB       BBB BBB         BB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BB     BBBBBBBBBBB       BB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BB  BBBB  BBBBBB  BBBB   BB
+  BB                       BB
+  BB                       BB
+  BB1                      BB
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+  `
+    },
+  
+    { //level3
+      jumpHeight: 2,
+      grid: `
+  BBBBBB    BBBBBBBB    BBBBB
+  BB                       BB
+  BB                       BB
+  BB                  +    BB
+  BB    +                  BB
+  BB  BBBB          BBBBB  BB
+  BB  B                 B  BB
+  BB  B                 B  BB
+  BB  B+             +  B  BB
+  BB  BBBBBB       BBBBBB  BB
+  BB  B                 B  BB
+  BB  B                 B  BB
+  BB  B                 B  BB
+  BB  BBBBBBBB   BBBBBBBB  BB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BBBBBB  BBB    BBB  BBBBBBB
+  BB                       BB
+  BB                       BB
+  BB1                      BB
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+  `
+    },
+    { //level4
+      jumpHeight: 2,
+      grid: `
+  BBBBBB    BBBBBBBB    BBBBB
+  BB   B                B  BB
+  BB   BBB           BBBB  BB
+  BB                  +    BB
+  BB    +                  BB
+  BB                       BB
+  BB  BBBBBBBB    BBBBBBB  BB
+  BB  B      B    B     B  BB
+  BB  B+     B    B    +B  BB
+  BB  BBB    B    B  BBBB  BB
+  BB         B    B        BB
+  BB         B    B        BB
+  BB   +               +   BB
+  BB  BBBB            BBB  BB
+  BB  B                 B  BB
+  BB  B                 B  BB
+  BB  BBBBBB  BBB  BBBBBB  BB
+  BB                       BB
+  BB                       BB
+  BB1                      BB
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+  `
+    },
+  
+  { //level5
     jumpHeight: 2,
     grid: `
-BBBBBBBBBB
-B        B
-B   +  + B
-BB BBB BBB
-B        B
-B        B
-B BBB BBBB
-B        B
-B1       B
-BBBBBBBBBB`
+BBBBBB    BBBBBBBB    BBBBB
+BB                       BB
+BB                       BB
+BB                       BB
+BB  B            +    B  BB
+BB  B BBBBBBBBBBBBBBBBB  BB
+BB                       BB
+BB                       BB
+BB  B         +       B  BB
+BB  BBBBBBBBBBBBBBBBB B  BB
+BB                       BB
+BB                       BB
+BB  B      +          B  BB
+BB  B BBBBBBBBBBBBBBBBB  BB
+BB                       BB
+BB                       BB
+BB  B   +             B  BB
+BB  BBBBBBBB   BBBBBBBB  BB
+BB                       BB
+BB                       BB
+BB1                      BB
+BBBBBBBBBBBBBBBBBBBBBBBBBBB
+`
+  },
+
+  { //level6
+    jumpHeight: 2,
+    grid: `
+  BBBBBB    BBBBBBBB    BBBBB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BB               +       BB
+  BB    BBBBBBBBBBBBBBBBBBBBB
+  BB                       BB
+  BB                       BB
+  BB            +          BB
+  BBBBBBBBBBBBBBBBBBBBB    BB
+  BB                       BB
+  BB                       BB
+  BB         +             BB
+  BB    BBBBBBBBBBBBBBBBBBBBB
+  BB                       BB
+  BB                       BB
+  BB      +                BB
+  BBBBBBBBBBB     BBBBBBBBBBB
+  BB                       BB
+  BB                       BB
+  BB1                      BB
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+`
+  },
+
+  { //level7
+    jumpHeight: 2,
+    grid: `
+  BBBBBB    BBBBBBBB    BBBBB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BB+                     +BB
+  BBBB                   BBBB
+  BB                       BB
+  BB                       BB
+  BB+                     +BB
+  BBBBBB               BBBBBB
+  BB                       BB
+  BB                       BB
+  BB+                     +BB
+  BBBBBBBBB         BBBBBBBBB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BB  BBBBBBBB   BBBBBBBB  BB
+  BB                       BB
+  BB                       BB
+  BB1                      BB
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+`
+  },
+  
+  { //level8
+    jumpHeight: 2,
+    grid: `
+  BBBBBB    BBBBBBBB    BBBBB
+  BB                       BB
+  BB                       BB
+  BB    B               B  BB
+  BB    B  +         +  B  BB
+  BB    B BB         BB B  BB
+  BB    B               B  BB
+  BB    B               B  BB
+  BB    B +          +  B  BB
+  BB    B BB         BB B  BB
+  BB    B               B  BB
+  BB                       BB
+  BB                       BB
+  BB      BBBBBBBBBBBBB    BB
+  BB                       BB
+  BB                       BB
+  BB                       BB
+  BB  BBBBBBBB   BBBBBBBB  BB
+  BB                       BB
+  BB                       BB
+  BB1                      BB
+  BBBBBBBBBBBBBBBBBBBBBBBBBBB
+`
   },
 
   {
